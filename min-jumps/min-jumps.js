be7111f4d71050151c279592a2570b0e7b00e19a
Steven Hejnas <shejnas91@gmail.com>
2020-04-09 10:26:11 -0400

    min jumps completed

--- min-jumps/min-jumps.js
+++ min-jumps/min-jumps.js
@@ -1,8 +1,33 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const minJumps = arr => {
+const minJumps = (arr) => {
+  if (arr.length === 0) return 0;
 
+  let idx = 0;
+  let counter = 0;
+
+  while (idx < arr.length - 1) {
+    //how far we can jump from where we are standing
+    let jumpDistance = arr[idx];
+
+    //can we jump to the end
+    if (jumpDistance + idx >= arr.length - 1) {
+      return ++counter;
+    } else {
+      //values of spaces that we can jump to
+      let jumpZone = arr.slice(idx + 1, idx + jumpDistance + 1);
+
+      //highest number that we can jump to
+      let nextHighestJump = Math.max(...jumpZone);
+
+      //jump! to the new index with the highest value
+      idx = idx + 1 + jumpZone.indexOf(nextHighestJump);
+
+      //record the jump
+      counter++;
+    }
+  }
 };
 
-module.exports = minJumps
+module.exports = minJumps;
